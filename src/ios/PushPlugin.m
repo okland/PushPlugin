ff4ade868488ef0fcb014da652681011cd95d8ea
Morten Henriksen <mh@gi2.dk>
2014-12-25 12:13:51 +0100

    Fix iOS 7 missing badge

--- src/ios/PushPlugin.m
+++ src/ios/PushPlugin.m
@@ -106,7 +106,10 @@
 #endif
     }
 
-    notificationTypes |= UIRemoteNotificationTypeNewsstandContentAvailability;
+    // Issue missing ios7 badge updates
+    // https://github.com/phonegap-build/PushPlugin/issues/365
+    // Fixed when newsstand contant
+    // notificationTypes |= UIRemoteNotificationTypeNewsstandContentAvailability;
 #if __IPHONE_OS_VERSION_MAX_ALLOWED >= 80000
     UserNotificationTypes |= UIUserNotificationActivationModeBackground;
 #endif
